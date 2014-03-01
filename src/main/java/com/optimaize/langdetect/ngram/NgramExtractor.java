6dd176e9e5f7ee3a9a8a274dff914d7b3b1dfa04
Fabian Kessler <fabian.kessler@optimaize.com>
2014-03-01 20:30:17 +0100

    Added new n-gram algorithm.

--- src/main/java/com/optimaize/langdetect/ngram/NgramExtractor.java
+++ src/main/java/com/optimaize/langdetect/ngram/NgramExtractor.java
@@ -1,19 +1,21 @@
 package com.optimaize.langdetect.ngram;
 
 import com.cybozu.labs.langdetect.util.NGram;
+import com.google.common.base.Strings;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Class for extracting n-grams out of a text.
  *
  * @author Fabian Kessler
+ * @author Nakatani Shuyo
  */
 public class NgramExtractor {
-
 
     public interface Filter {
         /**
@@ -36,6 +38,8 @@ public class NgramExtractor {
      *
      * TODO replace this algorithm with a simpler, faster one that uses less memory: only by position shifting. also, the returned list size
      * can be computed before making it (based on text length and number of n-grams).
+     *
+     * @author Nakatani Shuyo
      */
     @NotNull
     public static List<String> extractNGrams(@NotNull CharSequence text, @Nullable Filter filter) {
@@ -53,6 +57,32 @@ public class NgramExtractor {
             }
         }
         return list;
+    }
+
+
+    /**
+     * Creates the n-grams for a given text in the order they occur.
+     *
+     * <p>Example: extractSortedGrams("Foo bar", 2) => [Fo,oo,o , b,ba,ar]</p>
+     *
+     * <p>To have border grams, modify your input text. Example: instead of "foo" pass in " foo " or "$foo$".</p>
+     *
+     * @param  text
+     * @param  gramLength 1-n
+     * @return The grams, empty if the input was empty or if none for that gramLength fits.
+     */
+    @NotNull
+    public static List<String> extractGrams(@NotNull CharSequence text, int gramLength) {
+        int len = text.length();
+        int numGrams = len - (gramLength -1);
+        if (numGrams <= 0) {
+            return Collections.emptyList();
+        }
+        List<String> grams = new ArrayList<>(numGrams);
+        for (int pos=0; pos<numGrams; pos++) {
+            grams.add( text.subSequence(pos, pos+gramLength).toString() );
+        }
+        return grams;
     }
 
 }
--- src/test/java/com/optimaize/langdetect/ngram/NgramExtractorTest.java
+++ src/test/java/com/optimaize/langdetect/ngram/NgramExtractorTest.java
@@ -2,6 +2,7 @@ package com.optimaize.langdetect.ngram;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 import static org.junit.Assert.*;
 
@@ -11,12 +12,57 @@ import static org.junit.Assert.*;
 public class NgramExtractorTest {
 
     @Test
-    public void testExtractNGrams() throws Exception {
+    public void testExtractNGrams() {
         List<String> ngrams = NgramExtractor.extractNGrams("Foo bar", null);
         assertTrue(ngrams.contains("Foo"));
-        assertTrue(ngrams.contains(" Fo"));
         assertTrue(ngrams.contains("F"));
+        assertTrue(ngrams.contains(" Fo"));  //algorithm makes prefix-grams
+        assertFalse(ngrams.contains("ar ")); //algorithm does not make suffix-grams
         assertEquals(ngrams.size(), 18); //adapt when making changes to the extractor...
+    }
+
+    @Test
+    public void extractSortedGrams_1() {
+        String text = "Foo bar";
+        List<String> ngrams = NgramExtractor.extractGrams(text, 1);
+        assertEquals(ngrams.size(), text.length());
+        assertEquals(ngrams, Arrays.asList("F","o","o"," ","b","a","r"));
+    }
+
+    @Test
+    public void extractSortedGrams_2() {
+        String text = "Foo bar";
+        List<String> ngrams = NgramExtractor.extractGrams(text, 2);
+        assertEquals(ngrams.size(), text.length()-1);
+        assertEquals(ngrams, Arrays.asList("Fo","oo","o "," b","ba","ar"));
+    }
+
+    @Test
+    public void extractSortedGrams_3() {
+        String text = "Foo bar";
+        List<String> ngrams = NgramExtractor.extractGrams(text, 3);
+        assertEquals(ngrams.size(), text.length()-2);
+    }
+
+    @Test
+    public void extractSortedGrams_6() {
+        String text = "Foo bar";
+        List<String> ngrams = NgramExtractor.extractGrams(text, 6);
+        assertEquals(ngrams.size(), text.length()-5);
+    }
+
+    @Test
+    public void extractSortedGrams_7() {
+        String text = "Foo bar";
+        List<String> ngrams = NgramExtractor.extractGrams(text, 7);
+        assertEquals(ngrams.size(), text.length()-6);
+    }
+
+    @Test
+    public void extractSortedGrams_8() {
+        String text = "Foo bar";
+        List<String> ngrams = NgramExtractor.extractGrams(text, 8);
+        assertTrue(ngrams.isEmpty());
     }
 
 }
