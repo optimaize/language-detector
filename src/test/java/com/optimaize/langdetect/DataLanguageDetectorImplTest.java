3c24633dd61fdf8bdec25877b07f4c787323864b
Fabian Kessler <fabian.kessler@optimaize.com>
2016-02-19 22:55:31 +0200

    Added testng as dependency. Some tests use the @DataProvider, much nicer.

--- src/test/java/com/optimaize/langdetect/DataLanguageDetectorImplTest.java
+++ src/test/java/com/optimaize/langdetect/DataLanguageDetectorImplTest.java
@@ -1,51 +1,69 @@
 package com.optimaize.langdetect;
 
 import com.optimaize.langdetect.ngram.NgramExtractors;
+import com.optimaize.langdetect.profiles.LanguageProfile;
 import com.optimaize.langdetect.profiles.LanguageProfileReader;
 import com.optimaize.langdetect.text.CommonTextObjectFactories;
-import org.junit.Test;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 /**
+ * Uses all built-in language profiles and tests some simple clean phrases against them with expected outcome.
+ *
  * @author Fabian Kessler
  */
 public class DataLanguageDetectorImplTest {
 
-    @Test
-    public void shortTextAlgo() throws IOException {
-        LanguageDetector detector = LanguageDetectorBuilder.create(NgramExtractors.standard())
-                .shortTextAlgorithm(100)
-                .withProfiles(new LanguageProfileReader().readAllBuiltIn())
-                .build();
-        runTests(detector);
-    }
+    private final LanguageDetector shortDetector;
+    private final LanguageDetector longDetector;
 
-    @Test
-    public void longTextAlgo() throws IOException {
-        LanguageDetector detector = LanguageDetectorBuilder.create(NgramExtractors.standard())
+    public DataLanguageDetectorImplTest() throws IOException {
+        List<LanguageProfile> languageProfiles = new LanguageProfileReader().readAllBuiltIn();
+
+        shortDetector = LanguageDetectorBuilder.create(NgramExtractors.standard())
+                .shortTextAlgorithm(100)
+                .withProfiles(languageProfiles)
+                .build();
+
+        longDetector = LanguageDetectorBuilder.create(NgramExtractors.standard())
                 .shortTextAlgorithm(0)
                 .withProfiles(new LanguageProfileReader().readAllBuiltIn())
                 .build();
-        runTests(detector);
     }
 
-    /**
-     * Note: this works stable with getProbabilities(), the best language comes first.
-     * However, with detect(), it would not always work for Dutch.
-     * The short text algorithm returns a stable 0.99something, but not high enough for the current standards.
-     * The long text algorithm returns either a 0.9999something (often), or a much lower 0.7something (sometimes).
-     */
-    private void runTests(LanguageDetector detector) {
-        assertEquals(detector.getProbabilities(text("This is some English text.")).get(0).getLocale().getLanguage(), "en");
-        assertEquals(detector.getProbabilities(text("Ceci est un texte français.")).get(0).getLocale().getLanguage(), "fr");
-        assertEquals(detector.getProbabilities(text("Dit is een Nederlandse tekst.")).get(0).getLocale().getLanguage(), "nl");
-        assertEquals(detector.getProbabilities(text("Dies ist eine deutsche Text")).get(0).getLocale().getLanguage(), "de");
-        assertEquals(detector.getProbabilities(text("សព្វវចនាធិប្បាយសេរីសម្រាប់អ្នកទាំងអស់គ្នា។" +"នៅក្នុងវិគីភីឌាភាសាខ្មែរឥឡូវនេះមាន ១១៩៨រូបភាព សមាជិក១៥៣៣៣នាក់ និងមាន៤៥៨៣អត្ថបទ។")).get(0).getLocale().getLanguage(), "km");
-        assertEquals(detector.getProbabilities(text("Европа не трябва да стартира нов конкурентен маратон и изход с приватизация")).get(0).getLocale().getLanguage(), "bg");
+    @Test(dataProvider = "texts")
+    public void shortTextAlgo(String expectedLanguage, CharSequence text) throws IOException {
+        assertEquals(shortDetector.getProbabilities(text).get(0).getLocale().getLanguage(), expectedLanguage);
+
+        //not all are confident enough for the detect() method.
+        //assertEquals(shortDetector.detect(text).get().getLanguage(), expectedLanguage);
     }
+
+    @Test(dataProvider = "texts")
+    public void longTextAlgo(String expectedLanguage, CharSequence text) throws IOException {
+        assertEquals(longDetector.getProbabilities(text).get(0).getLocale().getLanguage(), expectedLanguage);
+
+        //not all are confident enough for the detect() method.
+        //assertEquals(longDetector.detect(text).get().getLanguage(), expectedLanguage);
+    }
+
+    @DataProvider
+    protected Object[][] texts() {
+        return new Object[][] {
+                {"en", text("This is some English text.")},
+                {"fr", text("Ceci est un texte français.")},
+                {"nl", text("Dit is een Nederlandse tekst.")},
+                {"de", text("Dies ist eine deutsche Text")},
+                {"km", text("សព្វវចនាធិប្បាយសេរីសម្រាប់អ្នកទាំងអស់គ្នា។" +"នៅក្នុងវិគីភីឌាភាសាខ្មែរឥឡូវនេះមាន ១១៩៨រូបភាព សមាជិក១៥៣៣៣នាក់ និងមាន៤៥៨៣អត្ថបទ។")},
+                {"bg", text("Европа не трябва да стартира нов конкурентен маратон и изход с приватизация")},
+        };
+    }
+
 
     private CharSequence text(CharSequence text) {
         return CommonTextObjectFactories.forDetectingShortCleanText().forText( text );
--- src/test/java/com/optimaize/langdetect/LanguageDetectorImplTest.java
+++ src/test/java/com/optimaize/langdetect/LanguageDetectorImplTest.java
@@ -7,55 +7,44 @@ import com.optimaize.langdetect.ngram.NgramExtractors;
 import com.optimaize.langdetect.profiles.LanguageProfile;
 import com.optimaize.langdetect.profiles.OldLangProfileConverter;
 import com.optimaize.langdetect.text.*;
-import org.junit.Test;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.testng.Assert.*;
 
 
 /**
+ * Basic tests for the LanguageDetectorImpl.
+ *
  * @author Fabian Kessler
  */
 public class LanguageDetectorImplTest {
 
-    @Test
-    public void german() throws IOException {
+    @Test(dataProvider = "confident")
+    public void confident(String expectedLanguage, CharSequence text) throws Exception {
         LanguageDetector languageDetector = makeNewDetector();
-        List<DetectedLanguage> result = languageDetector.getProbabilities("Dies ist eine deutsche Text");
+        List<DetectedLanguage> result = languageDetector.getProbabilities(text);
         DetectedLanguage best = result.get(0);
-        assertEquals(best.getLocale().getLanguage(), "de");
+        assertEquals(best.getLocale().getLanguage(), expectedLanguage);
         assertTrue(best.getProbability() >= 0.9999d);
     }
-
-    @Test
-    public void germanShort() throws IOException {
-        LanguageDetector languageDetector = makeNewDetector();
-        List<DetectedLanguage> result = languageDetector.getProbabilities("deutsche Text");
-        DetectedLanguage best = result.get(0);
-        assertEquals(best.getLocale().getLanguage(), "de");
-        assertTrue(best.getProbability() >= 0.9999d);
+    @DataProvider
+    protected Object[][] confident() {
+        return new Object[][] {
+                {"de", "Dies ist eine deutsche Text"},
+                {"de", "deutsche Text"},
+                {"de", CommonTextObjectFactories.forDetectingOnLargeText().create().append("deutsche Text").append(" ").append("http://www.github.com/")},
+        };
     }
 
-    @Test
-    public void germanShortWithUrl() throws IOException {
-        TextObjectFactory textObjectFactory = CommonTextObjectFactories.forDetectingOnLargeText();
-        TextObject inputText = textObjectFactory.create().append("deutsche Text").append(" ").append("http://www.github.com/");
-
-        LanguageDetector languageDetector = makeNewDetector();
-        List<DetectedLanguage> result = languageDetector.getProbabilities(inputText);
-        DetectedLanguage best = result.get(0);
-        assertEquals(best.getLocale().getLanguage(), "de");
-        assertTrue(best.getProbability() >= 0.9999d);
-    }
 
     private LanguageDetector makeNewDetector() throws IOException {
-        LanguageDetectorBuilder builder = LanguageDetectorBuilder.create(NgramExtractors.standard());
-        builder.shortTextAlgorithm(50);
-        builder.prefixFactor(1.5);
-        builder.suffixFactor(2.0);
+        LanguageDetectorBuilder builder = LanguageDetectorBuilder.create(NgramExtractors.standard())
+            .shortTextAlgorithm(50)
+            .prefixFactor(1.5)
+            .suffixFactor(2.0);
 
         LangProfileReader langProfileReader = new LangProfileReader();
         for (String language : ImmutableList.of("en", "fr", "nl", "de")) {
